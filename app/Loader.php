2eb8bf97e633c908672b0e1019d7d767b6f751a2
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2023-05-09 21:52:47 +0300

    added updater

--- app/Loader.php
+++ app/Loader.php
@@ -4,6 +4,7 @@ namespace BeycanPress\CryptoPay\Donation;
 
 use BeycanPress\CryptoPay\PluginHero\Hook;
 use BeycanPress\CryptoPay\PluginHero\Helpers;
+use BeycanPress\CryptoPay\PluginHero\Updater;
 use BeycanPress\CryptoPay\Pages\TransactionPage;
 
 class Loader
@@ -12,6 +13,16 @@ class Loader
 
     public function __construct()
     {
+        new Updater([
+            'requires' => '5.0',
+            'requires_php' => '7.4',
+            'plugin_version' => '1.0.0',
+            'plugin_file' =>  'cryptopay-donation/index.php',
+            'icons' => [
+                '2x' => plugin_dir_url(dirname(__FILE__, 2) . '/index.php') . '/assets/images/icon-256x256.png',
+                '1x' => plugin_dir_url(dirname(__FILE__, 2) . '/index.php') . '/assets/images/icon-128x128.png',
+            ]
+        ]);
         (new Models\DonationTransaction())->createTable();
 
         Hook::addFilter('models', function($models) {
